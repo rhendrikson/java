f40b1876ad2263fa7931df486c58cdfcd5f87ca8
rhendrikson <roger.hendrikson@yahoo.com>
2012-02-14 00:45:48 -0800

    Modified Prerequisites to first add classes with no prerequisites.

--- TopCoderProblems/src/topcoderproblems/Prerequisites.java
+++ TopCoderProblems/src/topcoderproblems/Prerequisites.java
@@ -13,6 +13,9 @@ import java.util.Map.Entry;
 public class Prerequisites {
 
     private HashMap<String, List<String>> classPrerequisites = new HashMap<>();
+//    private String[] schedule;
+//    private ClassComparator classComparator = new ClassComparator();
+    private TreeSet<String> schedule = new TreeSet<>(new ClassComparator());
 
     public String[] orderClasses(String[] param0) {
         setClassPrerequisites(param0);
@@ -20,22 +23,54 @@ public class Prerequisites {
             return new String[0];
         }
 
-        TreeSet<String> schedule = new TreeSet<>(new ClassComparator());
-        for (Entry<String, List<String>> map : classPrerequisites.entrySet()) {
-            for (String prerequisite : map.getValue()) {
-                if (!schedule.contains(prerequisite)) {
-                    schedule.add(prerequisite);
-                }
-            }
-            String className = map.getKey();
-            if (!schedule.contains(className)) {
-                schedule.add(className);
-            }
+//        TreeSet<String> schedule = new TreeSet<>(new ClassComparator());
+        
+        try {
+            addToSchedule(new ArrayList<>(classPrerequisites.keySet()));
+        } catch (IllegalArgumentException e) {
+            return new String[0];
         }
-
+        
+//        Set<String> classes = classPrerequisites.keySet();
+//        for (String className : classes) {
+//            List<String> prerequisites = classPrerequisites.get(className);
+//            try {
+//                for (String prerequisite : map.getValue()) {
+//                    if (!schedule.contains(prerequisite)) {
+//                        schedule.add(prerequisite);
+//                    }
+//                }
+//                String className = map.getKey();
+//                if (!schedule.contains(className)) {
+//                    schedule.add(className);
+//                }                
+//            } catch (IllegalArgumentException e) {
+//                return new String[0];
+//            }
+//        }
         return schedule.toArray(new String[0]);
+        
+//        schedule = new String[classPrerequisites.size()];
+//        for (Entry<String, List<String>> map : classPrerequisites.entrySet()) {
+//            setSchedule(map.getKey(), map.getValue());
+//        }
+//        return schedule;
     }
-
+    
+    private void addToSchedule(List<String> classes) {        
+        if (classes.isEmpty()) return;
+        
+        String className = classes.remove(0);
+        List<String> prerequisites = classPrerequisites.get(className);
+        if (prerequisites.isEmpty() && !schedule.contains(className)) {
+            schedule.add(className);
+            return;
+        }
+        
+        addToSchedule(prerequisites);
+        addToSchedule(classes);
+    }
+    
     private void setClassPrerequisites(String[] classes) {
         for (String classList : classes) {
             if (!classList.matches("^[A-Z]{3,4}[1-9]{1}[0-9]{2}:( [A-Z]{3,4}[1-9]{1}[0-9]{2})*$")) {
@@ -56,17 +91,54 @@ public class Prerequisites {
 
         @Override
         public int compare(String class1, String class2) {
-            int compareResult = class1.compareTo(class2);
-            
-            if (compareResult == 0) {
-                return 0;
+            List<String> class1Prerequisites = classPrerequisites.get(class1);
+            if (class1Prerequisites == null) {
+                throw new IllegalArgumentException();
             }
             
-//            if (classPrerequisites.get(class1).contains(class2)) {
-//                return -1;
-//            }
-
-            return compareResult;
+            boolean isClass2PrerequisiteOfClass1 = isClass2PrerequisiteOfClass1(class1, class2);
+            boolean isClass1PrerequisiteOfClass2 = isClass2PrerequisiteOfClass1(class2, class1);
+            
+            if (isClass2PrerequisiteOfClass1 && isClass1PrerequisiteOfClass2) {
+                throw new IllegalArgumentException();
+            }
+            
+            if (isClass2PrerequisiteOfClass1) {
+                return 1;
+            }
+            
+            if (isClass1PrerequisiteOfClass2) {
+                return -1;
+            }
+            
+            String classNumber1 = this.getClassNumber(class1);
+            String classNumber2 = this.getClassNumber(class2);
+            int compareResult = classNumber1.compareTo(classNumber2);
+            if (compareResult != 0) {
+                return compareResult;
+            }
+            
+            return class1.compareTo(class2);
         }
+        
+        private String getClassNumber(String className) {
+            return className.substring(className.length() - 3, className.length());
+        }
+        
+        private boolean isClass2PrerequisiteOfClass1(String class1, String class2) {
+            return classPrerequisites.get(class1).contains(class2);
+            
+//            if (classPrerequisites.get(class1).contains(class2)) {
+//                return true;
+//            }
+//            
+//            List<String> class2Prerequisites = classPrerequisites.get(class2);
+//            for (String prerequisite : class2Prerequisites) {
+//                return isClass2PrerequisiteOfClass1(class1, prerequisite);
+//            }
+//            
+//            return false;
+        }
+        
     }
 }
--- TopCoderProblems/src/topcoderproblems/TopCoderProblems.java
+++ TopCoderProblems/src/topcoderproblems/TopCoderProblems.java
@@ -4,6 +4,8 @@
  */
 package topcoderproblems;
 
+import java.util.Arrays;
+
 public class TopCoderProblems {
 
     /**
@@ -16,29 +18,26 @@ public class TopCoderProblems {
     
     private static void TestSquareDigits() {
         SquareDigits sd = new SquareDigits();
-        System.out.println(sd.smallestResult(2));
-        System.out.println(sd.smallestResult(10));
-        System.out.println(sd.smallestResult(19));
-        System.out.println(sd.smallestResult(85));
-        System.out.println(sd.smallestResult(112));
+        for (int number : new int[] { 2, 10, 19, 85, 112 }) {
+            System.out.format("Smallest result of square digits of %d: %d\r\n", number, sd.smallestResult(number));
+        }
     }
     
     private static void TestPrerequisites() {
-        Prerequisites prerequisites = new Prerequisites();
         String[][] testCases = new String[][]
         {
-            { "CS117:" },
-            { "cs117:" },
-            { "CS9E11:" },
-            { "CSE110: " },
-            { "CSE110: CSE101 " },
-            { "MATH211: MAM2222" },
-            { "MATH211: MAM22" },
-            { "ENGIN517: MATH211" },
-            { "CSE200: CSE100 CSE105", "CSE100:", "CSE105: CSE100" },
-            { "CSE121: CSE110", "CSE110:", "MATH122:" },
-            { "ENGL111: ENGL110", "ENGL110: ENGL111" },
-            { "ENGL111: ENGL110" },
+//            { "CS117:" },
+//            { "cs117:" },
+//            { "CS9E11:" },
+//            { "CSE110: " },
+//            { "CSE110: CSE101 " },
+//            { "MATH211: MAM2222" },
+//            { "MATH211: MAM22" },
+//            { "ENGIN517: MATH211" },
+//            { "CSE200: CSE100 CSE105", "CSE100:", "CSE105: CSE100" },
+//            { "CSE121: CSE110", "CSE110:", "MATH122:" },
+//            { "ENGL111: ENGL110", "ENGL110: ENGL111" },
+//            { "ENGL111: ENGL110" },
             { "CSE258: CSE244 CSE243 INTR100", "CSE221: CSE254 INTR100",
               "CSE254: CSE111 MATH210 INTR100", "CSE244: CSE243 MATH210 INTR100",
               "MATH210: INTR100", "CSE101: INTR100", "CSE111: INTR100",
@@ -46,11 +45,8 @@ public class TopCoderProblems {
               "INTR100:" }
         };
         for (String[] testCase : testCases) {
-            String[] schedule = prerequisites.orderClasses(testCase);
-            for (String className : schedule) {
-                System.out.print(className + " ");
-            }
-            System.out.println();
+            System.out.println("Test case: " + Arrays.deepToString(testCase));
+            System.out.println("Schedule: " + Arrays.deepToString(new Prerequisites().orderClasses(testCase)));
         }
     }
 }
