c6477055b0a11fad19b9df5d7995731be8fe4f22
rhendrikson <>
2012-02-15 17:08:56 -0800

    Changed algorithm.

--- TopCoderProblems/src/topcoderproblems/Prerequisites.java
+++ TopCoderProblems/src/topcoderproblems/Prerequisites.java
@@ -22,15 +22,34 @@ public class Prerequisites {
         if (classPrerequisites.isEmpty()) {
             return new String[0];
         }
-
-//        TreeSet<String> schedule = new TreeSet<>(new ClassComparator());
         
-        try {
-            addToSchedule(new ArrayList<>(classPrerequisites.keySet()));
-        } catch (IllegalArgumentException e) {
-            return new String[0];
+        // foreach class
+        //      foreach dependency
+        //          if dependency has dependencies
+        //              add dependencies in front of dependency
+        //      add class after all dependencies
+        
+        String[] mySchedule = new String[classPrerequisites.size()];
+        for (Entry<String, List<String>> map : classPrerequisites.entrySet()) {
+            for (String prerequisite : map.getValue()) {
+                addClassToSchedule(prerequisite);
+            }
+            addClassToSchedule(prerequisite);
         }
         
+        
+        
+
+//        TreeSet<String> schedule = new TreeSet<>(new ClassComparator());
+
+        for (Entry<String, List<String>> map : classPrerequisites.entrySet()) {
+            try {
+                addToSchedule(map.getKey(), map.getValue());
+            } catch (IllegalArgumentException e) {
+                return new String[0];
+            }
+        }
+
 //        Set<String> classes = classPrerequisites.keySet();
 //        for (String className : classes) {
 //            List<String> prerequisites = classPrerequisites.get(className);
@@ -49,35 +68,34 @@ public class Prerequisites {
 //            }
 //        }
         return schedule.toArray(new String[0]);
-        
+
 //        schedule = new String[classPrerequisites.size()];
 //        for (Entry<String, List<String>> map : classPrerequisites.entrySet()) {
 //            setSchedule(map.getKey(), map.getValue());
 //        }
 //        return schedule;
     }
-    
-    private void addToSchedule(List<String> classes) {        
-        if (classes.isEmpty()) return;
-        
-        String className = classes.remove(0);
-        List<String> prerequisites = classPrerequisites.get(className);
-        if (prerequisites.isEmpty() && !schedule.contains(className)) {
+
+    private void addToSchedule(String className, List<String> prerequisites) {
+        if (!schedule.contains(className)) {
             schedule.add(className);
             return;
         }
         
-        addToSchedule(prerequisites);
-        addToSchedule(classes);
+//        String prerequisite = prerequisites.remove(0);
+//
+//        for (String prerequisite : prerequisites) {
+//            addToSchedule(prerequisite, classPrerequisites.get(prerequisite));
+//        }
     }
-    
+
     private void setClassPrerequisites(String[] classes) {
         for (String classList : classes) {
             if (!classList.matches("^[A-Z]{3,4}[1-9]{1}[0-9]{2}:( [A-Z]{3,4}[1-9]{1}[0-9]{2})*$")) {
                 classPrerequisites.clear();
                 return;
             }
-            
+
             ArrayList<String> tokenizedClasses = new ArrayList<>();
             for (StringTokenizer tokenizer = new StringTokenizer(classList, " "); tokenizer.hasMoreTokens();) {
                 tokenizedClasses.add(tokenizer.nextToken());
@@ -95,39 +113,39 @@ public class Prerequisites {
             if (class1Prerequisites == null) {
                 throw new IllegalArgumentException();
             }
-            
+
             boolean isClass2PrerequisiteOfClass1 = isClass2PrerequisiteOfClass1(class1, class2);
             boolean isClass1PrerequisiteOfClass2 = isClass2PrerequisiteOfClass1(class2, class1);
-            
+
             if (isClass2PrerequisiteOfClass1 && isClass1PrerequisiteOfClass2) {
                 throw new IllegalArgumentException();
             }
-            
+
             if (isClass2PrerequisiteOfClass1) {
                 return 1;
             }
-            
+
             if (isClass1PrerequisiteOfClass2) {
                 return -1;
             }
-            
+
             String classNumber1 = this.getClassNumber(class1);
             String classNumber2 = this.getClassNumber(class2);
             int compareResult = classNumber1.compareTo(classNumber2);
             if (compareResult != 0) {
                 return compareResult;
             }
-            
+
             return class1.compareTo(class2);
         }
-        
+
         private String getClassNumber(String className) {
             return className.substring(className.length() - 3, className.length());
         }
-        
+
         private boolean isClass2PrerequisiteOfClass1(String class1, String class2) {
             return classPrerequisites.get(class1).contains(class2);
-            
+
 //            if (classPrerequisites.get(class1).contains(class2)) {
 //                return true;
 //            }
@@ -139,6 +157,5 @@ public class Prerequisites {
 //            
 //            return false;
         }
-        
     }
 }
