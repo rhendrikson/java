b61b71e4fa583258f1fb9ccd2755b99b79f86258
rhendrikson <>
2012-02-27 17:46:34 -0800

    Added method to generate permutations.

--- AlgorithmProblems/src/algorithmproblems/arraysandstrings/StringPermutation.java
+++ AlgorithmProblems/src/algorithmproblems/arraysandstrings/StringPermutation.java
@@ -5,6 +5,7 @@
 
 package algorithmproblems.arraysandstrings;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class StringPermutation {
@@ -27,5 +28,29 @@ public class StringPermutation {
         }
         
         return true;
+    }
+    
+    public static String[] getPermutations(String string) {
+        if (string.isEmpty()) return new String[0];
+        
+        if (string.length() == 1) {
+            return new String[] { string };
+        }
+        
+        if (string.length() == 2) {
+            return new String[]
+            {
+                string,
+                String.copyValueOf(new char[] { string.charAt(1), string.charAt(0) } )
+            };
+        }
+        
+        return concat(getPermutations(string), getPermutations(string));
+    }
+    
+    public static <T> T[] concat(T[] first, T[] second) {
+        T[] result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
     }
 }
--- AlgorithmProblems/test/algorithmproblems/arraysandstrings/StringPermutationTest.java
+++ AlgorithmProblems/test/algorithmproblems/arraysandstrings/StringPermutationTest.java
@@ -9,34 +9,67 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class StringPermutationTest {
-    
+
     @Test
     public void testEmptyString() {
         String string1 = "abc";
         String string2 = "";
         assertFalse(StringPermutation.isPermutation(string1, string2));
     }
-    
+
     @Test
     public void testValidStringPermutations() {
         String string1 = "abc";
-       
-        StringBuilder sb = new StringBuilder();
+        
         for (int i = 0; i < string1.length(); i++) {
-            sb.append(string1.substring(0, i));
-            sb.append(string1.charAt(i));
-            sb.append(string1.substring(i + 1, string1.length() - 1));
-            assertTrue(StringPermutation.isPermutation(string1, sb.toString()));
+            for (int j = 0; j < string1.length(); j++) {                
+                char[] string2 = new char[string1.length()];
+                
+                for (int k = 0; k < j; k++) {
+                    int copyFromIndex = k;
+                    if (k >= i) copyFromIndex++;
+                    string2[k] = string1.charAt(copyFromIndex);
+                }
+                
+                string2[j] = string1.charAt(i);
+                
+                for (int k = j + 1; k < string1.length(); k++) {
+                    int copyFromIndex = k - 1;
+                    if (k >= i) copyFromIndex++;
+                    string2[k] = string1.charAt(copyFromIndex);
+                }
+                System.out.println(String.copyValueOf(string2));
+            }
+        }
+        
+        System.out.println("Okay");
+        for (int i = 0; i < string1.length(); i++) {
+            for (int j = i; j < string1.length(); j++) {
+                StringBuilder sb = new StringBuilder();
+                if (j >= i) {
+                    sb.append(string1.substring(0, i));
+                    sb.append(string1.substring(i + 1, j + 1));
+                    sb.append(string1.charAt(i));
+                    sb.append(string1.substring(j + 1, string1.length()));
+                } else {
+                    sb.append(string1.substring(0, j));
+                    sb.append(string1.charAt(i));
+                    sb.append(string1.substring(j + 1, i));
+                    sb.append(string1.substring(i + 1, string1.length()));
+                }
+                System.out.println(sb.toString());
+                assertTrue(StringPermutation.isPermutation(string1, sb.toString()));
+            }
         }
     }
-    
+
     @Test
     public void testInvalidStringPermutation() {
         String string1 = "abc";
         String string2 = "def";
         assertFalse(StringPermutation.isPermutation(string1, string2));
     }
-    
+
     @Test
     public void testDifferingLengthsStringPermutation() {
         String string1 = "abc";
