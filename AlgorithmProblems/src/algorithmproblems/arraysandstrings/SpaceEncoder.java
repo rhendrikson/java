b3b2f28757ecbfa85d0b864034396e41f7d4e523
rhendrikson <>
2012-02-28 17:59:20 -0800

    Working on optimization of encoding algorithm.

--- AlgorithmProblems/src/algorithmproblems/arraysandstrings/SpaceEncoder.java
+++ AlgorithmProblems/src/algorithmproblems/arraysandstrings/SpaceEncoder.java
@@ -10,12 +10,37 @@ public class SpaceEncoder {
     private final static char[] SPACE_ENCODED = "%20".toCharArray();
     
     public static void encodeSpace(char[] characters) {
-        for (int i = 0; i < characters.length; i++) {
-            if (characters[i] == ' ') {
-                shiftRight(characters, i, SPACE_ENCODED.length);
-                System.arraycopy(SPACE_ENCODED, 0, characters, i, SPACE_ENCODED.length);
+        int numberOfSpaces = 0;
+        for (int i = characters.length - 2; i >= 0; i--) {
+            if (characters[i] == ' ' && 
+                characters[i + 1] != ' ') numberOfSpaces++;
+        }
+        // from the end
+        //     if character != ' '
+        //          copy char from index - (num of spaces * 2) to char[index]
+        //     else
+        //          copy encoded space chars
+        //          decrement num of spaces to copy by 1
+        
+        int numberOfSpacesCopied = 0;
+        for (int i = characters.length - 1; i >= 0; i--) {
+            int copyFromIndex = i - numberOfSpaces * 2;
+            int copyToIndex = i - numberOfSpacesCopied * 2;
+            if (characters[copyFromIndex] != ' ') {
+                characters[copyToIndex] = characters[copyFromIndex];
+            } else {
+                System.arraycopy(SPACE_ENCODED, 0, characters, i - SPACE_ENCODED.length + 1, SPACE_ENCODED.length);
+//                numberOfSpaces--;
+                numberOfSpacesCopied++;
             }
         }
+        
+//        for (int i = 0; i < characters.length; i++) {
+//            if (characters[i] == ' ') {
+//                shiftRight(characters, i, SPACE_ENCODED.length);
+//                System.arraycopy(SPACE_ENCODED, 0, characters, i, SPACE_ENCODED.length);
+//            }
+//        }
     }
 
     private static void shiftRight(char[] characters, int startIndex, int length) {
